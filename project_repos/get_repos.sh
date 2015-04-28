9781ab93b79e2c1d9805e4b3caf2508126c1ca0a
rjust <rjust.dev@gmail.com>
2015-04-28 10:50:10 -0700

    Added a script that downloads the project repositories if necessary -- we do not store the project repositories in the main defects4j repo.

---
+++ project_repos/get_repos.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+function clean {
+    rm -r \
+    closure-compiler.git \
+    commons-lang.git \
+    commons-math.git \
+    jfreechart \
+    joda-time.git \
+    README 
+}
+
+# The BSD version of stat does not support --version or -c
+stat --version &> /dev/null 
+if [[ $? ]]; then
+    # GNU version
+    cmd="stat -c %Y defects4j-repos.zip"
+else
+    # BSD version
+    cmd="stat -f %m defects4j-repos.zip"
+fi
+
+old=$($cmd)
+# Only download repos if the server has a newer file
+wget -N http://homes.cs.washington.edu/~rjust/defects4j/download/defects4j-repos.zip
+new=$($cmd)
+
+# Exit if no newer file is available
+[ $old -eq $new ] && exit 0
+
+# Remove old files
+clean
+
+# Extract new repos
+unzip -u defects4j-repos.zip && mv defects4j/project_repos/* . && rm -r defects4j
