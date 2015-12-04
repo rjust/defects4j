dfbfce16f8fae35ed069674c9437ff408c498e53
rjust <rjust.dev@gmail.com>
2015-12-04 12:59:16 -0800

    Updated init script and gitignore.

--- init.sh
+++ init.sh
@@ -5,8 +5,9 @@
 # - the project's version control repositories
 # - the Major mutation framework
 # - the supported test generation tools
-# - the supported code coverage tools
+# - the supported code coverage tools (TODO)
 ################################################################################
+# TODO: Major and the coverage tools should be moved to framework/lib
 
 # Check whether wget is available
 if ! wget --version > /dev/null 2>&1; then
@@ -26,6 +27,16 @@ mkdir -p $DIR_LIB_RT
 # Download project repositories if necessary
 #
 cd $DIR_REPOS && ./get_repos.sh
+
+#
+# Download Major
+#
+MAJOR_VERSION="1.1.6"
+MAJOR_URL="http://mutation-testing.org/downloads"
+MAJOR_ZIP="major-${MAJOR_VERSION}_jre7.zip"
+cd $BASE && wget -N $MAJOR_URL/$MAJOR_ZIP \
+         && unzip $MAJOR_ZIP \
+         && rm $MAJOR_ZIP
 
 #
 # Download EvoSuite
