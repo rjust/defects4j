5de9846ba8e8266ad10d9379973828a3c5ce98f5
rjust <rjust.dev@gmail.com>
2015-12-29 18:02:29 -0800

    Properly set up environment when executing any system command.

--- framework/core/Project.pm
+++ framework/core/Project.pm
@@ -1009,8 +1009,7 @@ sub _ant_call {
     -f $file or die "Build file does not exist: $file";
 
     # Set up environment before running ant
-    my $cmd = "export TZ='America/Los_Angeles'" .
-              " && cd $self->{prog_root}" .
+    my $cmd = " cd $self->{prog_root}" .
               " && ant -f $D4J_BUILD_FILE -Dd4j.home=$BASE_DIR -Dbasedir=$self->{prog_root} ${option_str} $target 2>&1";
     my $log;
     my $ret = Utils::exec_cmd($cmd, "Running ant ($target)", \$log);
--- framework/core/Utils.pm
+++ framework/core/Utils.pm
@@ -243,7 +243,7 @@ sub exec_cmd {
     @_ >= 2 or die $ARG_ERROR;
     my ($cmd, $descr, $log_ref) = @_;
     print(STDERR substr($descr . '.'x75, 0, 75), " ");
-    my $log = `$cmd`; my $ret = $?;
+    my $log = `export TZ='America/Los_Angeles'; $cmd`; my $ret = $?;
     $$log_ref = $log if defined $log_ref;
     if ($ret!=0) {
         print(STDERR "FAIL\n$log");
