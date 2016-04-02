24347e815b86707f87e964dba4737a16dfc62e95
rjust <rjust.dev@gmail.com>
2016-04-01 22:11:38 -0400

    Updated repository URL.

--- project_repos/get_repos.sh
+++ project_repos/get_repos.sh
@@ -28,7 +28,7 @@ else
     old=0
 fi
 # Only download repos if the server has a newer file
-wget -N http://homes.cs.washington.edu/~rjust/defects4j/download/$ARCHIVE
+wget -N http://people.cs.umass.edu/~rjust/defects4j/download/$ARCHIVE
 new=$($cmd)
 
 # Exit if no newer file is available
