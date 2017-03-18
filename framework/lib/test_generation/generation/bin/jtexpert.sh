38715774a031fe6e1b43fe6643a0b17a7027ef3e
rjust <rjust.dev@gmail.com>
2017-03-18 08:11:51 -0400

    Smaller improvements to test generation scripts.

--- framework/lib/test_generation/generation/bin/jtexpert.sh
+++ framework/lib/test_generation/generation/bin/jtexpert.sh
@@ -29,20 +29,28 @@ source $D4J_DIR_TESTGEN_LIB/bin/_tool.util
 #  -s: to show messages and errors thrown by the class under test
 #  -o: to override an existing test data file
 ################################################################################
+mkdir -p $D4J_DIR_OUTPUT
 for class in $(cat $D4J_FILE_TARGET_CLASSES); do
     file=$(get_abs_src_path $class)
     cmd="java -jar -Xms1G -Xmx1G $D4J_DIR_TESTGEN_LIB/jtexpert-current.jar \
         -cp $(get_project_cp) \
         -jf $file \
         -maxTime $D4J_CLASS_BUDGET \
-        -tp $D4J_DIR_OUTPUT \
+        -tp $D4J_DIR_WORKDIR/jte-tmp \
         -seed $D4J_SEED \
         -o"
 
+    # TODO: Check why JTExpert returns an error code even though it generates a
+    # test suite.
+    $cmd
+
     # Print the command that failed, if an error occurred.
-    if ! $cmd; then
-        echo
-        echo "FAILED: $cmd"
-        exit 1
-    fi
+    #if ! $cmd; then
+    #    echo
+    #    echo "FAILED: $cmd"
+    #    exit 1
+    #fi
+
+    # Copy the generated test cases to the specified output directory
+    cp -a $D4J_DIR_WORKDIR/jte-tmp/testcases/* $D4J_DIR_OUTPUT
 done
