c9a2ae58acda871f6508b220d2bb22769821f402
rjust <rjust.dev@gmail.com>
2017-03-19 14:12:58 -0400

    Updated T3 wrapper script: invoke a different main class to output JUnit test cases.

--- framework/lib/test_generation/generation/bin/t3.sh
+++ framework/lib/test_generation/generation/bin/t3.sh
@@ -22,14 +22,35 @@ source $D4J_DIR_TESTGEN_LIB/bin/_tool.util
 # Create the output directory
 mkdir -p $D4J_DIR_OUTPUT
 
+# Main class to invoke
+main=Sequenic.T3.DerivativeSuiteGens.Gen2.G2_forSBST2016
+
+# Arguments for G2_forSBST2016
+# arg0: generate
+# arg1: CUTname
+# arg2: CUTroot-directory
+# arg3: tracefile-directory
+# arg4: junitDir
+# arg5: time-budget
+# arg6: worklist-type:  standard/random/lowcovfirst
+# arg7: trace-refinement-heuristic: random/evo
+# arg8: the maximum number of times each test-target will be refined
+# arg9: number of CPU cores
+
 # Convert from seconds to millis
 budget=$(echo "$D4J_CLASS_BUDGET * 1000" | bc)
 for class in $(cat $D4J_FILE_TARGET_CLASSES); do
-    cmd="java -ea -cp $D4J_DIR_TESTGEN_LIB/t3-current.jar:$(get_project_cp) Sequenic.T3.T3Cmd \
-    --savedir $D4J_DIR_OUTPUT \
-    --timeout $budget \
-    --regressionmode \
-    $class"
+    cmd="java -ea -cp $D4J_DIR_TESTGEN_LIB/t3-current.jar:$(get_project_cp) $main \
+    generate \
+    $class \
+    $D4J_DIR_WORKDIR \
+    $D4J_DIR_OUTPUT \
+    $D4J_DIR_OUTPUT \
+    $budget \
+    random \
+    random \
+    1 \
+    1"
 
     # Print the command that failed, if an error occurred.
     if ! $cmd; then
