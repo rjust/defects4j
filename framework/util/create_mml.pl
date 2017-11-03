06e10c8e99dd3349c6cbff51e2a1a0d45baddc30
rjust <rjust.dev@gmail.com>
2017-11-03 14:04:02 -0400

    Fixed a typo.

--- framework/util/create_mml.pl
+++ framework/util/create_mml.pl
@@ -94,7 +94,7 @@ $BID =~ /^(\d+)$/ or die "Wrong bug id format (\\d+): $BID!";
 -e $CLASSES or die "File with classes to mutate does not exist: $CLASSES";
 
 # The mutation operators that should be enabled
-my @MU_OPS = ("AOR", "LOR","SOR", "COR", "ROR", "ORU", "LVR", "STD");
+my @MUT_OPS = ("AOR", "LOR","SOR", "COR", "ROR", "ORU", "LVR", "STD");
 
 my $mml_src = "$OUT_DIR/$BID.mml";
 my $mml_bin = "${mml_src}.bin";
