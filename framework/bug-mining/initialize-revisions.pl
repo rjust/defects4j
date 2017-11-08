9a6f6b2b90b8555837199b7a729c34d254999731
Tom Peck <tapeck@umass.edu>
2017-11-07 22:58:23 -0500

    its really a bid not a vid

--- framework/bug-mining/initialize-revisions.pl
+++ framework/bug-mining/initialize-revisions.pl
@@ -73,7 +73,7 @@ use Utils;
 my %cmd_opts;
 getopts('p:v:w:', \%cmd_opts) or pod2usage(1);
 
-my ($PID, $VID, $WORK_DIR) =
+my ($PID, $BID, $WORK_DIR) =
     ($cmd_opts{p},
      $cmd_opts{v},
      $cmd_opts{w}
@@ -82,8 +82,8 @@ my ($PID, $VID, $WORK_DIR) =
 pod2usage(1) unless defined $PID and defined $WORK_DIR; # $VID can be undefined
 
 # Check format of target version id
-if (defined $VID) {
-    $VID =~ /^(\d+)(:(\d+))?$/ or die "Wrong version id format ((\\d+)(:(\\d+))?): $VID!";
+if (defined $BID) {
+    $BID =~ /^(\d+)(:(\d+))?$/ or die "Wrong version id format ((\\d+)(:(\\d+))?): $BID!";
 }
 
 `mkdir -p $WORK_DIR/$PID/patches`; # create patches dir if it doesnt exist
@@ -101,37 +101,37 @@ $project->{prog_root} = $TMP_DIR;
 ############################### MAIN LOOP
 # figure out which IDs to run script for
 my @ids = $project->get_version_ids();
-if (defined $VID) {
-    if ($VID =~ /(\d+):(\d+)/) {
+if (defined $BID) {
+    if ($BID =~ /(\d+):(\d+)/) {
         @ids = grep { ($1 <= $_) && ($_ <= $2) } @ids;
     } else {
-        # single vid
-        @ids = grep { ($VID == $_) } @ids;
+        # single bid
+        @ids = grep { ($BID == $_) } @ids;
     }
 }
 
-foreach my $vid (@ids) {
-    printf ("%4d: $project->{prog_name}\n", $vid);
+foreach my $bid (@ids) {
+    printf ("%4d: $project->{prog_name}\n", $bid);
 
-    my $v1 = $project->lookup("${vid}b");
-    my $v2 = $project->lookup("${vid}f");
+    my $v1 = $project->lookup("${bid}b");
+    my $v2 = $project->lookup("${bid}f");
 
     # create local patch to get to buggy version, minimization wont matter here, that has to be done manually
     # will create a file in $WORK_DIR/$PID/patches
     # create the diff only on the src/
-    $project->export_diff($v2,$v1,"$WORK_DIR/$PID/patches/$vid.src.patch", "src/");
+    $project->export_diff($v2,$v1,"$WORK_DIR/$PID/patches/$bid.src.patch", "src/");
 
-    $project->checkout_vid("${vid}b");
+    $project->checkout_vid("${bid}b");
     $project->sanity_check();
-    $project->initialize_revision($v1, "${vid}b");
-    my ($src_b, $test_b) = ($project->src_dir("${vid}b"), $project->test_dir("${vid}b"));
+    $project->initialize_revision($v1, "${bid}b");
+    my ($src_b, $test_b) = ($project->src_dir("${bid}b"), $project->test_dir("${bid}b"));
 
-    $project->checkout_vid("${vid}f");
+    $project->checkout_vid("${bid}f");
     $project->sanity_check();
-    $project->initialize_revision($v2, "${vid}f");
-    my ($src_f, $test_f) = ($project->src_dir("${vid}f"), $project->test_dir("${vid}f"));
+    $project->initialize_revision($v2, "${bid}f");
+    my ($src_f, $test_f) = ($project->src_dir("${bid}f"), $project->test_dir("${bid}f"));
 
-    die "Source directories don't match for buggy and fixed revisions of $vid" unless $src_b eq $src_f;
-    die "Test directories don't match for buggy and fixed revisions of $vid" unless $test_b eq $test_f;
+    die "Source directories don't match for buggy and fixed revisions of $bid" unless $src_b eq $src_f;
+    die "Test directories don't match for buggy and fixed revisions of $bid" unless $test_b eq $test_f;
 }
 system("rm -rf $TMP_DIR");
