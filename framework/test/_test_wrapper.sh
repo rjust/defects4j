eaba9afb1ebf77fbec9ceec73fac08de86392c5b
Tom Peck <tapeck@umass.edu>
2017-12-12 08:59:17 -0500

    use arrays properly, make common tasks a function, only remove if it exists, route std out to null

--- framework/test/_test_wrapper.sh
+++ framework/test/_test_wrapper.sh
@@ -1,6 +1,5 @@
-fail_status_file=".test_failed.status"
-carton exec ./{$1} # start the test
-wait $! # wait for it
+fail_status_file=.test_failed.status
+carton exec ./${1}  > /dev/null # start the test, route STDOUT to null
 if [ $? -ne 0 ] ; then # if exit status is non zero  the fail status file
     echo $1 >> ${fail_status_file}
 fi
