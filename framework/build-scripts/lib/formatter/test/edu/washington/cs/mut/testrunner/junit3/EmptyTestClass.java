0f91c3b9b6f5a7833e1f174b27e2146960b95c96
Tom Peck <tapeck@umass.edu>
2017-12-22 12:13:40 -0500

    remove framework/build-scripts

--- framework/build-scripts/lib/formatter/test/edu/washington/cs/mut/testrunner/junit3/EmptyTestClass.java
+++
@@ -1,7 +0,0 @@
-package edu.washington.cs.mut.testrunner.junit3;
-
-import junit.framework.TestCase;
-
-public class EmptyTestClass extends TestCase {
-
-}
--- framework/build-scripts/lib/formatter/test/edu/washington/cs/mut/testrunner/junit4/EmptyTestClass.java
+++
@@ -1,7 +0,0 @@
-package edu.washington.cs.mut.testrunner.junit4;
-
-import junit.framework.TestCase;
-
-public class EmptyTestClass {
-
-}
