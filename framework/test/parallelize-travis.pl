f37ba601c0dbed74450e1aebbe1486c8aa349822
Tom Peck <tapeck@umass.edu>
2018-01-03 14:10:22 -0500

    cant do it that way, better add in my new script

---
+++ framework/test/parallelize-travis.pl
@@ -0,0 +1,23 @@
+use warnings;
+use strict;
+use File::Basename;
+use Cwd qw(abs_path);
+use Getopt::Std;
+use Pod::Usage;
+
+use lib (dirname(abs_path(__FILE__)) . "/../core/");
+use Constants;
+use Project;
+use DB;
+use Utils;
+
+############################## ARGUMENT PARSING
+my %cmd_opts;
+getopts('t:c:', \%cmd_opts) or pod2usage(1);
+
+my ($TRAVIS_CONFIG, $COMMIT_DB) =
+    ($cmd_opts{t},
+     $cmd_opts{c}
+    );
+
+pod2usage(1) unless defined $TRAVIS_CONFIG and defined $COMMIT_DB;
