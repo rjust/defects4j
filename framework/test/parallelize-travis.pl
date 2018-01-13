21ac6950ce7c296c87f6b95aa51f3826b6cb01a1
Tom Peck <tapeck@umass.edu>
2018-01-12 22:03:19 -0500

    made a new weird syntax for multiple commit dbs

--- framework/test/parallelize-travis.pl
+++ framework/test/parallelize-travis.pl
@@ -6,9 +6,10 @@ Break down any bugs into tests
 
 Travis config file
 
-=item B<-c C<commit-db>>
+=item B<-c C<commit-db>,C<commit-db>>
 
-Commit db to read bugs from
+List of commit-db's to read bugs from in the form of B<Project,path-to-commit-db:Project2,path-to-commit-db-2>
+example B<perl framework/test/parallelize-travis.pl -c Time,framework/projects/Time/commit-db:Lang,framework/projects/Lang/commit-db -t .travis.yml>
 
 =cut
 
@@ -29,27 +30,68 @@ use Utils;
 my %cmd_opts;
 getopts('t:c:', \%cmd_opts) or pod2usage(1);
 
-my ($TRAVIS_CONFIG, $COMMIT_DB) =
-    ($cmd_opts{t},
-     $cmd_opts{c}
-    );
+my ($TRAVIS_CONFIG, $STR_DATABASES) =
+($cmd_opts{t},
+  $cmd_opts{c}
+);
 
-pod2usage(1) unless defined $TRAVIS_CONFIG and defined $COMMIT_DB;
+pod2usage(1) unless defined $TRAVIS_CONFIG and defined $STR_DATABASES;
+
+# break down databases
+my @db_list = split(/:/,$STR_DATABASES);
+my %project_pairs;
+
+foreach (@db_list) {
+  my @pair = split(/,/);
+  # build hash of databases { project => filename }
+  $project_pairs{$pair[0]} = $pair[1];
+}
 
 # go through the commit-db and collect bug id's
-my $dbh = DBI->connect("dbi:CSV:", undef, undef, {
-                       f_dir      => dirname($COMMIT_DB),
-                       RaiseError => 1,
-                       skip_first_row => 0,
-                      })
-          or die "Cannot connect: $DBI::errstr";
-my $sth = $dbh->prepare("SELECT * FROM " . $dbh->quote(basename($COMMIT_DB))) or die $dbh->errstr;
-$sth->execute() or die $sth->errstr;
-
-my %row;
-while(%row = $sth->fetchrow_hashref) {
-  print "$row{PROJECT} $row{ID}";
-  exit;
+my %bugs;
+foreach my $key (keys %project_pairs) {
+  $bugs{$key} = read_commit_db($project_pairs{$key});
 }
 
 # add bug id's correct section of travis config
+
+
+=pod
+
+  read_commit_db(csv_filename)
+
+=head1
+
+Read the commit-db and pull bugs out of it
+
+=cut
+
+sub read_commit_db {
+  my $csv_filename = shift;
+
+  my $dbh = DBI->connect("dbi:CSV:", undef, undef, {
+      f_dir      => dirname($csv_filename),
+      RaiseError => 1,
+      skip_first_row => 0 }
+  )
+    or die "Cannot connect: $DBI::errstr";
+  $dbh->{csv_tables}{basename($csv_filename)} = {
+    eol         => "\n",
+    sep_char    => ",",
+    quote_char  => undef,
+    escape_char => undef,
+    col_names   => [qw( bug_id rev1 rev2 )],
+  };
+
+  my $sth = $dbh->prepare("SELECT * FROM " . $dbh->quote(basename($csv_filename))) or die $dbh->errstr;
+  $sth->execute() or die $sth->errstr;
+
+  my @bugs;
+  my %row;
+  while(%row = $sth->fetchrow_hashref) {
+    push(@bugs, $row{bug_id});
+  }
+  return @bugs;
+}
+
+1;
