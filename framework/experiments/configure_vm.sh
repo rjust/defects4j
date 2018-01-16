38ba23b258b7d5014eb346fbfef96f43cf348cea
Gregory Gay <greg@greggay.com>
2018-01-16 14:12:54 -0500

    Fix to config script

--- framework/experiments/configure_vm.sh
+++ framework/experiments/configure_vm.sh
@@ -7,7 +7,7 @@ git clone https://github.com/Greg4cr/defects4j.git
 cd defects4j
 git checkout mcc-experiment
 ./init.sh
-cp framework/lib/test_generation/generation/evosuite-1.0.3.jar framework/lib/test_generation/generation/evosuite-current.jar
-cp framework/lib/test_generation/runtime/evosuite-standalone-runtime-1.0.3.jar framework/lib/test_generation/runtime/evosuite-rt.jar
-export PATH=$PATH:/home/greg/defects4j/framework/bin
-echo 'export PATH=$PATH:/home/greg/defects4j/framework/bin' >> /home/greg/.bashrc
+cp framework/lib/test_generation/generation/evosuite-1.0.5.jar framework/lib/test_generation/generation/evosuite-current.jar
+cp framework/lib/test_generation/runtime/evosuite-standalone-runtime-1.0.5.jar framework/lib/test_generation/runtime/evosuite-rt.jar
+export PATH=$PATH:$HOME/defects4j/framework/bin
+echo 'export PATH=$PATH:/home/greg/defects4j/framework/bin' >> $HOME/.bashrc
