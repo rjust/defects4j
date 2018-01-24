84817b8d3b401d8cc9bb2ff0c5e7b6559ef1c495
Tom Peck <tapeck@umass.edu>
2018-01-24 15:26:52 -0500

    add init_debug.sh which should help debug why evosuite isnt initing

---
+++ init_debug.sh
@@ -0,0 +1,121 @@
+#!/usr/bin/env bash
+#
+################################################################################
+# This script initializes Defects4J. In particular, it downloads and sets up:
+# - the project's version control repositories
+# - the Major mutation framework
+# - the supported test generation tools
+# - the supported code coverage tools (TODO)
+################################################################################
+# TODO: Major and the coverage tools should be moved to framework/lib
+
+# Check whether wget is available
+if ! wget --version > /dev/null 2>&1; then
+    echo "Couldn't find wget to download dependencies. Please install wget and re-run this script."
+    exit 1
+fi
+
+# Directories for project repositories and external libraries
+BASE="$(cd $(dirname $0); pwd)"
+DIR_REPOS="$BASE/project_repos"
+DIR_LIB_GEN="$BASE/framework/lib/test_generation/generation"
+DIR_LIB_RT="$BASE/framework/lib/test_generation/runtime"
+DIR_LIB_GRADLE="$BASE/framework/lib/build_systems/gradle"
+mkdir -p "$DIR_LIB_GEN" && mkdir -p "$DIR_LIB_RT" && mkdir -p "$DIR_LIB_GRADLE"
+
+################################################################################
+#
+# Download project repositories if necessary
+#
+echo "Setting up project repositories ... "
+cd "$DIR_REPOS" && ./get_repos.sh
+
+################################################################################
+#
+# Download Major
+#
+echo
+echo "Setting up Major ... "
+MAJOR_VERSION="1.3.2"
+MAJOR_URL="http://mutation-testing.org/downloads"
+MAJOR_ZIP="major-${MAJOR_VERSION}_jre7.zip"
+cd "$BASE" && wget -nv -N "$MAJOR_URL/$MAJOR_ZIP" \
+           && unzip -o "$MAJOR_ZIP" > /dev/null \
+           && rm "$MAJOR_ZIP" \
+           && cp major/bin/.ant major/bin/ant
+
+################################################################################
+#
+# Download EvoSuite
+#
+echo
+echo "Setting up EvoSuite ... "
+EVOSUITE_VERSION="0.2.0"
+EVOSUITE_URL="http://www.evosuite.org/files"
+EVOSUITE_JAR="evosuite-${EVOSUITE_VERSION}.jar"
+EVOSUITE_RT_JAR="evosuite-standalone-runtime-${EVOSUITE_VERSION}.jar"
+cd "$DIR_LIB_GEN"
+echo "enter dir_lib_gen"
+if [ ! -f "$EVOSUITE_JAR" ] ; then
+  echo "get evo suite"
+  wget -nv "$EVOSUITE_URL/$EVOSUITE_JAR"
+fi
+cd "$DIR_LIB_RT"
+echo "enter dir lib rt"
+if [ ! -f "$EVOSUITE_RT_JAR" ] ; then
+  echo "get evosuite rt"
+  wget -nv "$EVOSUITE_URL/$EVOSUITE_RT_JAR"
+fi
+
+# Set symlinks for the supported version of EvoSuite
+echo "set sym link for lib gen jar"
+ln -sf "$DIR_LIB_GEN/$EVOSUITE_JAR" "$DIR_LIB_GEN/evosuite-current.jar"
+echo "set sym link for lib rj jar"
+ln -sf "$DIR_LIB_RT/$EVOSUITE_RT_JAR" "$DIR_LIB_RT/evosuite-rt.jar"
+
+################################################################################
+#
+# Download Randoop
+#
+echo
+echo "Setting up Randoop ... "
+RANDOOP_VERSION="3.1.5"
+RANDOOP_URL="https://github.com/randoop/randoop/releases/download/v${RANDOOP_VERSION}"
+RANDOOP_JAR="randoop-all-${RANDOOP_VERSION}.jar"
+cd "$DIR_LIB_GEN" && [ ! -f "$RANDOOP_JAR" ] \
+                  && wget -nv "$RANDOOP_URL/$RANDOOP_JAR"
+# Set symlink for the supported version of Randoop
+ln -sf "$DIR_LIB_GEN/$RANDOOP_JAR" "$DIR_LIB_GEN/randoop-current.jar"
+
+################################################################################
+#
+# Download build system dependencies
+#
+echo
+echo "Setting up Gradle dependencies ... "
+GRADLE_ZIP=defects4j-gradle.zip
+# The BSD version of stat does not support --version or -c
+if stat --version &> /dev/null; then
+    # GNU version
+    cmd="stat -c %Y $GRADLE_ZIP"
+else
+    # BSD version
+    cmd="stat -f %m $GRADLE_ZIP"
+fi
+
+cd "$DIR_LIB_GRADLE"
+if [ -e $GRADLE_ZIP ]; then
+    old_ts=$($cmd)
+else
+    old_ts=0
+fi
+# Only download archive if the server has a newer file
+wget -N http://people.cs.umass.edu/~rjust/defects4j/download/$GRADLE_ZIP
+new=$($cmd)
+
+# Update gradle versions if a newer archive was available
+[ "$old" != "$new" ] && unzip -q -u $GRADLE_ZIP
+
+cd "$BASE"
+echo
+echo "Defects4J successfully initialized."
