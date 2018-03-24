92bc39bf0067e04659b0c0e979e8bd0749d72b71
ypzheng <ypzheng@umass.edu>
2018-03-24 18:23:06 -0400

    Updated analyzer test and test timeout option.

--- framework/lib/Build_Analyzer/test/TestAll.java
+++ framework/lib/Build_Analyzer/test/TestAll.java
@@ -9,43 +9,43 @@ import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 public class TestAll {
-	
+
 	@Test
 	public void testMain() throws UnsupportedEncodingException, FileNotFoundException, IOException {
-		
+
 		Driver.main(new String[] {"test/projects/commons-lang","test/actual_output","maven-build.xml"});
 	}
-	
+
 	@Test
 	public void testTarget() throws IOException {
-		File expected = new File("test/expected_output/targets.txt");
-		File actual = new File("test/actual_output/targets.txt");
+		File expected = new File("test/expected_output/targets");
+		File actual = new File("test/actual_output/targets");
 		boolean isTwoEqual = FileUtils.contentEquals(expected, actual);
 		assertTrue(isTwoEqual);
 	}
-	
+
 	@Test
 	public void testIncludes() throws IOException {
-		File expected = new File("test/expected_output/includes.txt");
-		File actual = new File("test/actual_output/includes.txt");
+		File expected = new File("test/expected_output/includes");
+		File actual = new File("test/actual_output/includes");
 		boolean isTwoEqual = FileUtils.contentEquals(expected, actual);
 		assertTrue(isTwoEqual);
 	}
-	
+
 	@Test
 	public void testExcludes() throws IOException {
-		File expected = new File("test/expected_output/excludes.txt");
-		File actual = new File("test/actual_output/excludes.txt");
+		File expected = new File("test/expected_output/excludes");
+		File actual = new File("test/actual_output/excludes");
 		boolean isTwoEqual = FileUtils.contentEquals(expected, actual);
 		assertTrue(isTwoEqual);
 	}
-	
+
 	@Test
 	public void testTestList() throws IOException {
-		File expected = new File("test/expected_output/developer-included-tests.txt");
-		File actual = new File("test/actual_output/developer-included-tests.txt");
+		File expected = new File("test/expected_output/developer-included-tests");
+		File actual = new File("test/actual_output/developer-included-tests");
 		boolean isTwoEqual = FileUtils.contentEquals(expected, actual);
 		assertTrue(isTwoEqual);
 	}
-	
+
 }
