6d2bfb454b292cb6cbe5ebb7ef9459755e477242
Tom Peck <tom@tompeck.me>
2018-04-15 12:05:27 -0400

    Revert "Revert "Ignore any other special params on commit db when looking for revs""
    This reverts commit d6e2d89d069c59a346d8814c337bf523ea366d02.

--- framework/bug-mining/merge-commit-db.pl
+++ framework/bug-mining/merge-commit-db.pl
@@ -75,7 +75,7 @@ my $existing_commits_count = scalar @existing_commits;
 while (my $line = <STDIN>) {
     chomp $line;
     next unless $line;
-    $line =~ /(?:\d+,){0,1}(.+),(.+)/; # allows a bugid but will ignore it (this is just a quick adaptation in case you feed a commit-db into this script)
+    $line =~ /(?:\d+,){0,1}(.+),(.+)(?:,.+)*/; # allows a bugid and other random params but will ignore them (this is just a quick adaptation in case you feed a commit-db into this script)
     # check commits src dir if they are non empty before merging
     unless(`git --git-dir=$git_dir --no-pager diff --binary $2 $1 -- src/ src/` eq "") { # buggy to fixed
         unshift @new_commits, "$1,$2";  # unshift places line at the front
