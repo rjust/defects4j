a66658c81b1d131b3043c46897c6ccd16e99cf5e
Mark Roberts <markro@cs.washington.edu>
2018-05-22 10:19:00 -0700

    fix indentation

--- framework/util/show_coverage.pl
+++ framework/util/show_coverage.pl
@@ -56,13 +56,13 @@ my $help = 0;
 my $details = 0;
 my $man = 0;
 
-    # Parse options and print usage if there is a syntax error,
-    # or if usage was explicitly requested.
-    GetOptions('help|?' => \$help, details => \$details, man => \$man) or pod2usage(2);
-    pod2usage(1) if $help;
-    pod2usage(-verbose => 2) if $man;
-    # Check for too many filenames
-    pod2usage("$0: Too many files given.\n")  if (@ARGV > 1);
+# Parse options and print usage if there is a syntax error,
+# or if usage was explicitly requested.
+GetOptions('help|?' => \$help, details => \$details, man => \$man) or pod2usage(2);
+pod2usage(1) if $help;
+pod2usage(-verbose => 2) if $man;
+# Check for too many filenames
+pod2usage("$0: Too many files given.\n")  if (@ARGV > 1);
 
 # if the number of tests to be run is modified in randoop_coverage.sh,
 # then the expected_test_count will need to be modified to match.
@@ -73,54 +73,54 @@ my $tot_exec = 0;
 my $tot_fail = 0;
 my $filename = '/tmp/test_d4j/coverage';
 
-    print(strftime("\nToday's date: %Y-%m-%d %H:%M:%S", localtime), "\n");
+print(strftime("\nToday's date: %Y-%m-%d %H:%M:%S", localtime), "\n");
 
-    if (@ARGV == 1) {
-        $filename = $ARGV[0];
-    }
+if (@ARGV == 1) {
+    $filename = $ARGV[0];
+}
 
-    open(my $fh, '<', $filename)
-      or die "Could not open file '$filename'. $!.\n";
-    printf("Processing file: %s\n", $filename);
-    print(strftime("Created: %Y-%m-%d %H:%M:%S", localtime((stat($fh))[9])), "\n");
+open(my $fh, '<', $filename)
+  or die "Could not open file '$filename'. $!.\n";
+printf("Processing file: %s\n", $filename);
+print(strftime("Created: %Y-%m-%d %H:%M:%S", localtime((stat($fh))[9])), "\n");
 
-    if ($details) {
-        print "\nTest    Lines     Total    %", "\n";
-        print "name    covered   lines    coverage", "\n";
-    }
+if ($details) {
+    print "\nTest    Lines     Total    %", "\n";
+    print "name    covered   lines    coverage", "\n";
+}
 
-    # read all input lines into an array and
-    # sort it to get consistent output
-    my @lines = <$fh>;
+# read all input lines into an array and
+# sort it to get consistent output
+my @lines = <$fh>;
 
-    foreach (sort(@lines)) {
-        chomp;
-        my @fields = split /,/;
-            if (@fields == 0) {
-                # do nothing for a blank line
-            } elsif ($fields[0] eq "project_id") {
-                # do nothing for a header line
+foreach (sort(@lines)) {
+    chomp;
+    my @fields = split /,/;
+    if (@fields == 0) {
+        # do nothing for a blank line
+    } elsif ($fields[0] eq "project_id") {
+        # do nothing for a header line
+    } else {
+        $test_count += 1;
+        $tot_line += $fields[4];
+        $tot_exec += $fields[5];
+        if ($details) {
+            if ($fields[4] != 0) {
+                printf("%s: %d %d %.2f\n", $fields[0].$fields[1], $fields[5], $fields[4], $fields[5]/$fields[4]);
             } else {
-                $test_count += 1;
-                $tot_line += $fields[4];
-                $tot_exec += $fields[5];
-                if ($details) {
-                    if ($fields[4] != 0) {
-                        printf("%s: %d %d %.2f\n", $fields[0].$fields[1], $fields[5], $fields[4], $fields[5]/$fields[4]);
-                    } else {
-                        printf("%s: failed\n", $fields[0].$fields[1]);
-                        $tot_fail += 1;
-                    }
-                }
+                printf("%s: failed\n", $fields[0].$fields[1]);
+                $tot_fail += 1;
             }
+        }
     }
+}
 
-    if ($expected_test_count < $test_count) {
-        die "More test results than expected!";
-    }
-    $tot_fail += $expected_test_count - $test_count;
+if ($expected_test_count < $test_count) {
+    die "More test results than expected!";
+}
+$tot_fail += $expected_test_count - $test_count;
 
-    printf("\nNumber of tests: %d, %d failed\n", $expected_test_count, $tot_fail);
-    print "Total lines: ", $tot_line, "\n";
-    print "Lines executed: ", $tot_exec, "\n";
-    printf("Coverage: %.2f\n", $tot_exec/$tot_line);
+printf("\nNumber of tests: %d, %d failed\n", $expected_test_count, $tot_fail);
+print "Total lines: ", $tot_line, "\n";
+print "Lines executed: ", $tot_exec, "\n";
+printf("Coverage: %.2f\n", $tot_exec/$tot_line);
