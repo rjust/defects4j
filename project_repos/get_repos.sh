cd24c5d9c1bb1aef84a75783ac7767cccff94c37
Bo Yang <boyang9602@gmail.com>
2019-09-18 17:43:03 -0400

    remove comparsion between  and  to prevent: https://github.com/rjust/defects4j/issues/190

--- project_repos/get_repos.sh
+++ project_repos/get_repos.sh
@@ -13,26 +13,8 @@ clean() {
     README 
 }
 
-# The BSD version of stat does not support --version or -c
-if stat --version &> /dev/null; then
-    # GNU version
-    cmd="stat -c %Y $ARCHIVE"
-else
-    # BSD version
-    cmd="stat -f %m $ARCHIVE"
-fi
-
-if [ -e $ARCHIVE ]; then
-    old=$($cmd)
-else
-    old=0
-fi
 # Only download repos if the server has a newer file
 wget -N http://people.cs.umass.edu/~rjust/defects4j/download/$ARCHIVE
-new=$($cmd)
-
-# Exit if no newer file is available
-[ "$old" == "$new" ] && exit 0
 
 # Remove old files
 clean
