6ef7443af0ee36311891f9a5f1a2aae926ad3827
rjust <rjust.dev@gmail.com>
2020-02-18 06:21:11 -0800

    Update get-repos script to handle new projects.

--- project_repos/get_repos.sh
+++ project_repos/get_repos.sh
@@ -8,10 +8,21 @@ ARCHIVE=defects4j-repos.zip
 clean() {
     rm -rf \
     closure-compiler.git \
+    commons-cli.git \
+    commons-codec.git \
+    commons-collections.git \
+    commons-compress.git \
+    commons-csv.git \
+    commons-jxpath.git \
     commons-lang.git \
     commons-math.git \
+    gson.git \
+    jackson-core.git \
+    jackson-databind.git \
+    jackson-dataformat-xml \
     jfreechart \
     joda-time.git \
+    jsoup.git \
     mockito.git \
     README 
 }
@@ -49,6 +60,7 @@ else
 fi
 # Only download repos if the server has a newer file
 download_url "https://defects4j.org/downloads/$ARCHIVE"
+
 new=$($cmd)
 
 # Exit if no newer file is available
