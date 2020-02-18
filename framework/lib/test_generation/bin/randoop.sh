7c48f92e2f3ef7285c3f2e292a721990e3e1f833
Michael Ernst <mernst@cs.washington.edu>
2020-02-18 08:47:06 -0800

    Reduce whitespace

--- framework/lib/test_generation/bin/randoop.sh
+++ framework/lib/test_generation/bin/randoop.sh
@@ -57,18 +57,18 @@ printf " " >&2
 
 # Build the test-generation command
 cmd="java -ea -classpath $project_cp:$D4J_DIR_TESTGEN_LIB/randoop-current.jar \
-          -Xbootclasspath/a:$D4J_DIR_TESTGEN_LIB/replacecall-current.jar \
-          -javaagent:$D4J_DIR_TESTGEN_LIB/replacecall-current.jar \
-          -javaagent:$D4J_DIR_TESTGEN_LIB/covered-class-current.jar \
-    randoop.main.Main gentests \
-          --classlist=$D4J_DIR_WORKDIR/classes.randoop \
-          --require-covered-classes=$D4J_FILE_TARGET_CLASSES \
-          --junit-output-dir=$D4J_DIR_OUTPUT \
-          --randomseed=$D4J_SEED \
-          --time-limit=$D4J_TOTAL_BUDGET \
-          --regression-test-basename=$REG_BASE_NAME \
-          --error-test-basename=$ERR_BASE_NAME \
-          $add_config"
+  -Xbootclasspath/a:$D4J_DIR_TESTGEN_LIB/replacecall-current.jar \
+  -javaagent:$D4J_DIR_TESTGEN_LIB/replacecall-current.jar \
+  -javaagent:$D4J_DIR_TESTGEN_LIB/covered-class-current.jar \
+randoop.main.Main gentests \
+  --classlist=$D4J_DIR_WORKDIR/classes.randoop \
+  --require-covered-classes=$D4J_FILE_TARGET_CLASSES \
+  --junit-output-dir=$D4J_DIR_OUTPUT \
+  --randomseed=$D4J_SEED \
+  --time-limit=$D4J_TOTAL_BUDGET \
+  --regression-test-basename=$REG_BASE_NAME \
+  --error-test-basename=$ERR_BASE_NAME \
+  $add_config"
 
 # Print the command that failed, if an error occurred.
 if ! $cmd; then
