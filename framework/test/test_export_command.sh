65860338f111354a36699468f80183b5cd6abe95
Greg4cr <greg@greggay.com>
2020-02-21 09:33:13 +0100

    Per-project control for test_export_command (fixes #292)

--- framework/test/test_export_command.sh
+++ framework/test/test_export_command.sh
@@ -2,6 +2,11 @@
 ################################################################################
 #
 # This script tests the export command.
+# By default, executed for all projects.
+# Test one or more specific projects by appending "-p" arguments.
+# Examples: 
+# * Verify Lang:    ./test_export_command.sh -p Lang
+# * Verify Lang and Collections: ./test_export_command.sh -p Lang -p Collections
 #
 ################################################################################
 
@@ -36,114 +41,147 @@ _run_export_command() {
 # ------------------------------------------------------------------- Test Cases
 
 test_ExportTestClassesDir() {
+    local pid=$1
     local test_dir="$TMP_DIR/test_ExportTestClassesDir"
     rm -rf "$test_dir"; mkdir -p "$test_dir"
 
-    # Iterate over all projects
-    for pid in $(cd "$BASE_DIR/framework/core/Project" && ls *.pm | sed -e 's/\.pm//g'); do
-        # Iterate over all bugs
-        local bids=$(cut -f1 -d',' "$BASE_DIR/framework/projects/$pid/commit-db")
-        for bid in $bids; do
-            local work_dir="$test_dir/$pid/$bid"
-            mkdir -p "$work_dir"
+    # Iterate over all bugs
+    local bids=$(cut -f1 -d',' "$BASE_DIR/framework/projects/$pid/commit-db")
+    for bid in $bids; do
+        local work_dir="$test_dir/$pid/$bid"
+        mkdir -p "$work_dir"
 
-            defects4j checkout -p "$pid" -v "${bid}b" -w "$work_dir" || die "Checkout of $pid-$bid has failed"
+        defects4j checkout -p "$pid" -v "${bid}b" -w "$work_dir" || die "Checkout of $pid-$bid has failed"
 
-            local test_classes_dir=""
-            test_classes_dir=$(_run_export_command "$work_dir" "dir.bin.tests")
-            if [ $? -ne 0 ]; then
-                die "Export command of $pid-$bid has failed"
+        local test_classes_dir=""
+        test_classes_dir=$(_run_export_command "$work_dir" "dir.bin.tests")
+        if [ $? -ne 0 ]; then
+            die "Export command of $pid-$bid has failed"
+        fi
+
+        local expected=""
+        if [ "$pid" == "Chart" ]; then
+            expected="build-tests"
+        elif [ "$pid" == "Cli" ]; then
+            expected="target/test-classes"
+        elif [ "$pid" == "Closure" ]; then
+            expected="build/test"
+        elif [ "$pid" == "Codec" ]; then
+            if [ "$bid" -ge "1" ] && [ "$bid" -le "16" ]; then
+                expected="target/tests"
+            elif [ "$bid" -ge "17" ] && [ "$bid" -le "18" ]; then
+                expected="target/test-classes"
             fi
-
-            local expected=""
-            if [ "$pid" == "Chart" ]; then
-                expected="build-tests"
-            elif [ "$pid" == "Cli" ]; then
-                expected="target/test-classes"
-            elif [ "$pid" == "Closure" ]; then
-                expected="build/test"
-            elif [ "$pid" == "Codec" ]; then
-                if [ "$bid" -ge "1" ] && [ "$bid" -le "16" ]; then
-                    expected="target/tests"
-                elif [ "$bid" -ge "17" ] && [ "$bid" -le "18" ]; then
-                    expected="target/test-classes"
-                fi
-            elif [ "$pid" == "Collections" ]; then
-                if [ "$bid" -ge "1" ] && [ "$bid" -le "21" ]; then
-                    expected="build/tests"
-                elif [ "$bid" -ge "22" ] && [ "$bid" -le "28" ]; then
-                    expected="target/tests"
-                fi
-            elif [ "$pid" == "Compress" ]; then
-                expected="target/test-classes"
-            elif [ "$pid" == "Csv" ]; then
-                expected="target/test-classes"
-            elif [ "$pid" == "Gson" ]; then
-                expected="target/test-classes"
-            elif [ "$pid" == "JacksonCore" ]; then
-                expected="target/test-classes"
-            elif [ "$pid" == "JacksonDatabind" ]; then
-                expected="target/test-classes"
-            elif [ "$pid" == "JacksonXml" ]; then
-                expected="target/test-classes"
-            elif [ "$pid" == "Jsoup" ]; then
-                expected="target/test-classes"
-            elif [ "$pid" == "JxPath" ]; then
-                expected="target/test-classes"
-            elif [ "$pid" == "Lang" ]; then
-                if [ "$bid" -ge "1" ] && [ "$bid" -le "20" ]; then
-                    expected="target/tests"
-                elif [ "$bid" -ge "21" ] && [ "$bid" -le "41" ]; then
-                    expected="target/test-classes"
-                elif [ "$bid" -ge "42" ] && [ "$bid" -le "65" ]; then
-                    expected="target/tests"
-                fi
-            elif [ "$pid" == "Math" ]; then
-                expected="target/test-classes"
-            elif [ "$pid" == "Mockito" ]; then
-                if [ "$bid" -ge "1" ] && [ "$bid" -le "11" ]; then
-                    expected="build/classes/test"
-                elif [ "$bid" -ge "12" ] && [ "$bid" -le "17" ]; then
-                    expected="target/test-classes"
-                elif [ "$bid" -ge "18" ] && [ "$bid" -le "21" ]; then
-                    expected="build/classes/test"
-                elif [ "$bid" -ge "22" ] && [ "$bid" -le "38" ]; then
-                    expected="target/test-classes"
-                fi
-            elif [ "$pid" == "Time" ]; then
-                if [ "$bid" -ge "1" ] && [ "$bid" -le "11" ]; then
-                    expected="target/test-classes"
-                elif [ "$bid" -ge "12" ] && [ "$bid" -le "27" ]; then
-                    expected="build/tests"
-                fi
+        elif [ "$pid" == "Collections" ]; then
+            if [ "$bid" -ge "1" ] && [ "$bid" -le "21" ]; then
+                expected="build/tests"
+            elif [ "$bid" -ge "22" ] && [ "$bid" -le "28" ]; then
+                expected="target/tests"
             fi
+        elif [ "$pid" == "Compress" ]; then
+            expected="target/test-classes"
+        elif [ "$pid" == "Csv" ]; then
+            expected="target/test-classes"
+        elif [ "$pid" == "Gson" ]; then
+            expected="target/test-classes"
+        elif [ "$pid" == "JacksonCore" ]; then
+            expected="target/test-classes"
+        elif [ "$pid" == "JacksonDatabind" ]; then
+            expected="target/test-classes"
+        elif [ "$pid" == "JacksonXml" ]; then
+            expected="target/test-classes"
+        elif [ "$pid" == "Jsoup" ]; then
+            expected="target/test-classes"
+        elif [ "$pid" == "JxPath" ]; then
+            expected="target/test-classes"
+        elif [ "$pid" == "Lang" ]; then
+            if [ "$bid" -ge "1" ] && [ "$bid" -le "20" ]; then
+                expected="target/tests"
+            elif [ "$bid" -ge "21" ] && [ "$bid" -le "41" ]; then
+                expected="target/test-classes"
+            elif [ "$bid" -ge "42" ] && [ "$bid" -le "65" ]; then
+                expected="target/tests"
+            fi
+        elif [ "$pid" == "Math" ]; then
+            expected="target/test-classes"
+        elif [ "$pid" == "Mockito" ]; then
+            if [ "$bid" -ge "1" ] && [ "$bid" -le "11" ]; then
+                 expected="build/classes/test"
+            elif [ "$bid" -ge "12" ] && [ "$bid" -le "17" ]; then
+                expected="target/test-classes"
+            elif [ "$bid" -ge "18" ] && [ "$bid" -le "21" ]; then
+                expected="build/classes/test"
+            elif [ "$bid" -ge "22" ] && [ "$bid" -le "38" ]; then
+                expected="target/test-classes"
+            fi
+        elif [ "$pid" == "Time" ]; then
+            if [ "$bid" -ge "1" ] && [ "$bid" -le "11" ]; then
+                expected="target/test-classes"
+            elif [ "$bid" -ge "12" ] && [ "$bid" -le "27" ]; then
+                expected="build/tests"
+            fi
+        fi
 
-            # Assert
-            [ "$test_classes_dir" == "$expected" ] || die "Actual test classes directory of $pid-$bid ('$test_classes_dir') is not the one expected ('$expected')"
+        # Assert
+        [ "$test_classes_dir" == "$expected" ] || die "Actual test classes directory of $pid-$bid ('$test_classes_dir') is not the one expected ('$expected')"
 
-            # Clean up
-            rm -rf "$work_dir"
-        done
+        # Clean up
+        rm -rf "$work_dir"
     done
 
     # Clean up
     rm -rf "$test_dir"
 }
 
-# Run all test cases (and log all results), regardless of whether errors occur
-HALT_ON_ERROR=0
 
-test_ExportTestClassesDir || die "Test 'test_ExportTestClassesDir' has failed!"
+# Print usage message and exit
+usage() {
+    local known_pids=$(cd "$BASE_DIR"/framework/core/Project && ls *.pm | sed -e 's/\.pm//g')
+    echo "usage: $0 -p <project id>"
+    echo "Project ids:"
+    for pid in $known_pids; do
+        echo "  * $pid"
+    done
+    exit 1
+}
 
-HALT_ON_ERROR=1
+# Check arguments
+while getopts ":p:" opt; do
+    case $opt in
+        p) PIDS="$PIDS $OPTARG"
+            ;;
+        \?)
+            echo "Unknown option: -$OPTARG" >&2
+            usage
+            ;;
+        :)
+            echo "No argument provided: -$OPTARG." >&2
+            usage
+            ;;
+  esac
+done
 
-# Print a summary of what went wrong
-if [ "$ERROR" -ne "0" ]; then
-    printf '=%.s' $(seq 1 80) 1>&2
-    echo 1>&2
-    echo "The following errors occurred:" 1>&2
-    cat $LOG 1>&2
+# If no arguments provided, iterate over all projects
+if [ "$PIDS" == "" ]; then
+    PIDS=$(cd "$BASE_DIR/framework/core/Project" && ls *.pm | sed -e 's/\.pm//g')
 fi
 
-# Indicate whether an error occurred
-exit "$ERROR"
+for PID in $PIDS; do
+    # Run all test cases (and log all results), regardless of whether errors occur
+    HALT_ON_ERROR=0
+
+    test_ExportTestClassesDir $PID || die "Test 'test_ExportTestClassesDir' has failed!"
+
+    HALT_ON_ERROR=1
+
+    # Print a summary of what went wrong
+    if [ "$ERROR" -ne "0" ]; then
+        printf '=%.s' $(seq 1 80) 1>&2
+        echo 1>&2
+        echo "The following errors occurred:" 1>&2
+        cat $LOG 1>&2
+    fi
+
+    # Indicate whether an error occurred
+    exit "$ERROR"
+done
