23b483ea744678e9ec76747d62fc77b79f7ef9e7
Michael Ernst <mernst@cs.washington.edu>
2020-11-09 07:41:16 -0800

    Define function download_url_and_unzip

--- init.sh
+++ init.sh
@@ -28,7 +28,7 @@ fi
 HOST_URL="https://defects4j.org/downloads"
 
 # Directories for project repositories and external libraries
-BASE="$(cd $(dirname $0); pwd)"
+BASE="$(cd "$(dirname "$0")"; pwd)"
 DIR_REPOS="$BASE/project_repos"
 DIR_LIB_GEN="$BASE/framework/lib/test_generation/generation"
 DIR_LIB_RT="$BASE/framework/lib/test_generation/runtime"
@@ -45,23 +45,46 @@ if [ "$(uname)" = "Darwin" ] ; then
   function timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
 fi
 
-# Download the remote resource to a local file of the same name, if the
-# remote resource is newer.  Works around connections that hang.  Takes a
-# single command-line argument, a URL.
+# Download the remote resource to a local file of the same name.
+# Takes a single command-line argument, a URL.
+# Skips the download if the remote resource is newer.
+# Works around connections that hang.
 download_url() {
-    echo "Downloading $@"
-    if [ "$(uname)" = "Darwin" ] ; then
-        wget -nv -N "$@"
-    else
-	BASENAME=`basename ${@: -1}`
-	if [ -f $BASENAME ]; then
-	    ZBASENAME="-z $BASENAME"
-	else
-	    ZBASENAME=""
-	fi
-	timeout 300 curl -s -S -R -L -O $ZBASENAME "$@" || (echo "retrying curl $@" && rm -f "$BASENAME" && curl -R -L -O "$@")
+    if [ "$#" -ne 1 ]; then
+        echo "Illegal number of arguments"
     fi
-    echo "Downloaded $@"
+    URL=$1
+    echo "Downloading $URL"
+    if [ "$(uname)" = "Darwin" ] ; then
+        wget -nv -N "URL" && echo "Downloaded URL"
+    else
+        BASENAME=$(basename ${URL: -1})
+        if [ -f $BASENAME ]; then
+            ZBASENAME="-z $BASENAME"
+        else
+            ZBASENAME=""
+        fi
+        (timeout 300 curl -s -S -R -L -O $ZBASENAME "URL" || (echo "retrying curl URL" && rm -f "$BASENAME" && curl -R -L -O "URL")) && echo "Downloaded URL"
+    fi
+}
+
+# Download the remote resource and unzip it.
+# Takes a single command-line argument, a URL.
+# Skips the download if the local file of the same name is newer.
+# Works around connections that hang and corrupted downloads.
+download_url_and_unzip() {
+    if [ "$#" -ne 1 ]; then
+        echo "Illegal number of arguments"
+    fi
+    URL=$1
+    BASENAME=$(basename ${URL: -1})
+    download_url "$1"
+    if ! unzip -o "$BASENAME" > /dev/null ; then
+        echo "retrying download and unzip"
+        rm -rf "$BASENAME"
+        download_url "$URL"
+        unzip -o "$BASENAME"
+    fi
 }
 
 # Get time of last data modification of a file
@@ -103,8 +126,7 @@ echo "Setting up Major ... "
 MAJOR_VERSION="1.3.4"
 MAJOR_URL="https://mutation-testing.org/downloads"
 MAJOR_ZIP="major-${MAJOR_VERSION}_jre7.zip"
-cd "$BASE" && download_url "$MAJOR_URL/$MAJOR_ZIP" \
-           && unzip -o "$MAJOR_ZIP" > /dev/null \
+cd "$BASE" && download_url_and_unzip "$MAJOR_URL/$MAJOR_ZIP" \
            && rm "$MAJOR_ZIP" \
            && cp major/bin/.ant major/bin/ant
 
@@ -136,11 +158,7 @@ RANDOOP_ZIP="randoop-${RANDOOP_VERSION}.zip"
 RANDOOP_JAR="randoop-all-${RANDOOP_VERSION}.jar"
 REPLACECALL_JAR="replacecall-${RANDOOP_VERSION}.jar"
 COVEREDCLASS_JAR="covered-class-${RANDOOP_VERSION}.jar"
-(cd "$DIR_LIB_GEN" && download_url "$RANDOOP_URL/$RANDOOP_ZIP" \
-                   && unzip -q $RANDOOP_ZIP) \
- || (cd "$DIR_LIB_GEN" && rm -f $RANDOOP_ZIP \
-                   && download_url "$RANDOOP_URL/$RANDOOP_ZIP" \
-                   && unzip -q $RANDOOP_ZIP)
+(cd "$DIR_LIB_GEN" && download_url_and_unzip "$RANDOOP_URL/$RANDOOP_ZIP")
 # Set symlink for the supported version of Randoop
 (cd "$DIR_LIB_GEN" && ln -sf "randoop-${RANDOOP_VERSION}/$RANDOOP_JAR" "randoop-current.jar")
 (cd "$DIR_LIB_GEN" && ln -sf "randoop-${RANDOOP_VERSION}/$REPLACECALL_JAR" "replacecall-current.jar")
