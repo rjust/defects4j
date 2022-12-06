efe7d65727ccbc60da94c399af170ecf2a30cc90
ryanfeatherman <ryan.featherman3@gmail.com>
2022-12-05 21:19:25 -0800

    fixed compress

--- framework/test/test_verify_bugs.sh
+++ framework/test/test_verify_bugs.sh
@@ -104,11 +104,17 @@ for bid in $(echo $BUGS); do
         defects4j checkout -p $PID -v "$vid" -w "$work_dir" || die "checkout: $PID-$vid"
         case $PID in
             Cli)
+                # doesn't always exist
                 sed -i '' "s/source=\"1\.[1-5]\"/source=\"1.6\"/" $work_dir/maven-build.xml
                 sed -i '' "s/target=\"1\.[1-5]\"/target=\"1.6\"/" $work_dir/maven-build.xml
+                # only needed for 22
                 sed -i '' "s/source=\"1\.[1-5]\"/source=\"1.6\"/" $work_dir/build.xml
                 sed -i '' "s/target=\"1\.[1-5]\"/target=\"1.6\"/" $work_dir/build.xml
                 ;;
+            Compress)
+                # doesn't always exist
+                sed -i '' "s/source=\"1\.[1-5]\"/source=\"1.6\"/" $work_dir/maven-build.xml
+                sed -i '' "s/target=\"1\.[1-5]\"/target=\"1.6\"/" $work_dir/maven-build.xml
         esac
         defects4j compile -w "$work_dir" || die "compile: $PID-$vid"
         defects4j test $TEST_FLAG -w "$work_dir" || die "run relevant tests: $PID-$vid"
