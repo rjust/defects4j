7b3eda9c79e3de4c7468ab1a638bc4325d71fb44
Michael Ernst <mernst@cs.washington.edu>
2024-05-28 15:58:48 -0700

    Undo a change

--- framework/lib/test_generation/bin/randoop.sh
+++ framework/lib/test_generation/bin/randoop.sh
@@ -65,7 +65,8 @@ ERR_BASE_NAME=ErrorTest
 #shellcheck disable=SC2153 # D4J_DIR_TESTGEN_LIB is not a typo of D4J_DIR_TESTGEN_BIN
 version=$(java -cp "$D4J_DIR_TESTGEN_LIB/randoop-current.jar" randoop.main.Main | head -1)
 printf "\n(%s)" "$version" >&2
-printf ".%.0s" \{1..expr 73 - length "$version"\} >&2
+# shellcheck disable=SC1083
+printf ".%.0s" {1..expr 73 - length "$version"} >&2
 printf " " >&2
 
 # The most common package in file $D4J_FILE_TARGET_CLASSES.
