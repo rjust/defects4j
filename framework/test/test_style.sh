aee220a4cc133135fa65e70f923dc861f43e5f74
Michael Ernst <mernst@cs.washington.edu>
2024-05-28 13:42:36 -0700

    Check Perl style

---
+++ framework/test/test_style.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+TOPLEVEL="$(git rev-parse --show-toplevel)"
+
+cd "$TOPLEVEL" || (echo "Cannot cd to $TOPLEVEL" && exit 1)
+
+find . -name '*.pm' -o -name '*.pl' -print0 | xargs -0 -n1 perl -Mstrict -Mdiagnostics -cw
