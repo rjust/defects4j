d8167a33a69c70394e04333db9dcaabebbf1d057
Michael Ernst <mernst@cs.washington.edu>
2024-08-04 08:44:48 -0700

    Add comment

--- framework/test/test_style.sh
+++ framework/test/test_style.sh
@@ -4,6 +4,7 @@ TOPLEVEL="$(git rev-parse --show-toplevel)"
 
 cd "$TOPLEVEL" || (echo "Cannot cd to $TOPLEVEL" && exit 1)
 
+# Check style of Perl scripts.
 find . \( -name '*.pm' -o -name '*.pl' \) -print0 | xargs -0 -n1 perl -Mstrict -Mdiagnostics -cw
 
 # Check style of sh scripts.
