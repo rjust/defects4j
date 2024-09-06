e86f814842979912320b3c43049e3d502ed83979
Michael Ernst <mernst@cs.washington.edu>
2024-09-06 15:53:08 -0700

    Address shellcheck warnings

--- framework/test/test_mutation_cmd.sh
+++ framework/test/test_mutation_cmd.sh
@@ -16,7 +16,7 @@ source test.include
 
 # Print usage message and exit
 usage() {
-    local known_pids=$(defects4j pids)
+    local known_pids; known_pids=$(defects4j pids)
     echo "usage: $0 -p <project id> [-b <bug id> ... | -b <bug id range> ... ]"
     echo "Project ids:"
     for pid in $known_pids; do
@@ -31,9 +31,9 @@ while getopts ":p:b:" opt; do
         p) PID="$OPTARG"
             ;;
         b) if [[ "$OPTARG" =~ ^[0-9]*\.\.[0-9]*$ ]]; then
-                BUGS="$BUGS $(eval echo {$OPTARG})"
+               BUGS="$BUGS $(eval echo \{"$OPTARG"\})"
            else
-                BUGS="$BUGS $OPTARG"
+               BUGS="$BUGS $OPTARG"
            fi
             ;;
         \?)
@@ -59,20 +59,20 @@ init
 
 # Run all bugs, unless otherwise specified
 if [ "$BUGS" == "" ]; then
-    BUGS="$(get_bug_ids $BASE_DIR/framework/projects/$PID/$BUGS_CSV_ACTIVE)"
+    BUGS="$(get_bug_ids "$BASE_DIR/framework/projects/$PID/$BUGS_CSV_ACTIVE")"
 fi
 
 # Create log file
-script_name=$(echo $script | sed 's/\.sh$//')
-LOG="$TEST_DIR/${script_name}$(printf '_%s_%s' $PID $$).log"
-OUT_DIR="$TEST_DIR/${script_name}$(printf '_%s_%s' $PID $$).mutation_summary"
-mkdir -p $OUT_DIR
+script_name_without_sh=${script//.sh/}
+LOG="$TEST_DIR/${script_name_without_sh}$(printf '_%s_%s' "$PID" $$).log"
+OUT_DIR="$TEST_DIR/${script_name_without_sh}$(printf '_%s_%s' "$PID" $$).mutation_summary"
+mkdir -p "$OUT_DIR"
 
 # Reproduce all bugs (and log all results), regardless of whether errors occur
 HALT_ON_ERROR=0
 
 test_dir=$(mktemp -d)
-for bid in $(echo $BUGS); do
+for bid in $BUGS; do
     # Skip all bug ids that do not exist in the active-bugs csv
     if ! grep -q "^$bid," "$BASE_DIR/framework/projects/$PID/$BUGS_CSV_ACTIVE"; then
         warn "Skipping bug ID that is not listed in active-bugs csv: $PID-$bid"
@@ -82,12 +82,12 @@ for bid in $(echo $BUGS); do
     # Test mutation analysis for the fixed version only.
     vid="${bid}f"
     work_dir="$test_dir/$PID-$vid"
-    defects4j checkout -p $PID -v "$vid" -w "$work_dir" || die "checkout: $PID-$vid"
+    defects4j checkout -p "$PID" -v "$vid" -w "$work_dir" || die "checkout: $PID-$vid"
     defects4j mutation -w "$work_dir" || die "mutation: $PID-$vid"
 
     cat "$work_dir/summary.csv" > "$OUT_DIR/$bid"
 done
-rm -rf $test_dir
+rm -rf "$test_dir"
 HALT_ON_ERROR=1
 
 # Print a summary of what went wrong
@@ -95,7 +95,7 @@ if [ $ERROR != 0 ]; then
     printf '=%.s' $(seq 1 80) 1>&2
     echo 1>&2
     echo "The following errors occurred:" 1>&2
-    cat $LOG 1>&2
+    cat "$LOG" 1>&2
 fi
 
 # Indicate whether an error occurred
